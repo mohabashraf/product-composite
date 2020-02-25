06b3aadaafefc022ad295c22fb93b1f679c5e14f
mohabashraf <mohabashraf466@gmail.com>
2020-02-25 22:30:51 +0200

    add changes

--- product-compose/src/main/java/com/productcompose/productcompose/ProductComposeApplication.java
+++ product-compose/src/main/java/com/productcompose/productcompose/ProductComposeApplication.java
@@ -2,9 +2,16 @@ package com.productcompose.productcompose;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ProductComposeApplication {
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductComposeApplication.class, args);
