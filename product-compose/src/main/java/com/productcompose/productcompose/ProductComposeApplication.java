2f5acbe5f6b387ad90f31e2e0bff46cab242cad4
mohabashraf <mohabashraf466@gmail.com>
2020-02-25 12:42:54 +0200

    add the util and the api

---
+++ product-compose/src/main/java/com/productcompose/productcompose/ProductComposeApplication.java
@@ -0,0 +1,13 @@
+package com.productcompose.productcompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductComposeApplication.class, args);
+	}
+
+}
---
+++ product-compose/src/test/java/com/productcompose/productcompose/ProductComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productcompose.productcompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
